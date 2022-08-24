b06ecaba44b1b916ca1b4be376ac83d483c561d7
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2022-08-24 11:11:03 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -26,11 +26,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-#iris = datasets.load_iris()
 iris = pd.read_csv('https://raw.githubusercontent.com/feizhi98/Demo1/main/IRIS.csv')
-st.write("hi:",iris.head())
-X = df 
-Y = iris.species
+X = iris.data
+Y = iris.target
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -39,7 +37,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(Y)
+st.write(iris.target_names)
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
